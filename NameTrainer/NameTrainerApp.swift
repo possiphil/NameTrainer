d96530fb85d0fa7187dda508ff04f4a08d60f09b
Philipp Sanktjohanser <venture@mein.gmx>
2023-01-16 06:10:05 +0100

    Initial Commit

---
+++ NameTrainer/NameTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NameTrainerApp.swift
+//  NameTrainer
+//
+//  Created by Philipp Sanktjohanser on 16.01.23.
+//
+
+import SwiftUI
+
+@main
+struct NameTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
